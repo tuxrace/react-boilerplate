09d82ebe9c38cce5c6cd8f56c6e59680a6f93a52
Gerard Banasig <gerardb@istackmanila.com>
2017-09-06 17:31:33 +0800

    modify and added feathers for backend

--- server/index.js
+++ server/index.js
@@ -8,10 +8,11 @@ const setup = require('./middlewares/frontendMiddleware');
 const isDev = process.env.NODE_ENV !== 'production';
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const resolve = require('path').resolve;
+const feathersApi = require('../api/app.js')
 const app = express();
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
-// app.use('/api', myApi);
+app.use('/api', feathersApi);
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
